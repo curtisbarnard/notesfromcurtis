4efaa2fd1902033a1318bd66b2ae37e5d85d7d65
Curtis Barnard <curtis.barnard.jr@gmail.com>
2024-12-27 14:54:05 -0800

    Update DNS settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://curtisbarnard.github.io',
-  base: '/notesfromcurtis',
+  site: 'https://notes.curtisbarnard.com',
   integrations: [mdx()],
 });
