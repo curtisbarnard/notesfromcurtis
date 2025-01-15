80900fb01edf81053e1aac15f2a271c1b72b0147
Curtis Barnard Jr. <100104319+curtisbarnard@users.noreply.github.com>
2025-01-14 22:47:08 -0800

    Add Mikoda Morning Market post and tweak RSS feed (#7)
    * Add CinemaImage component and tweak site body text color
    
    * Add Double4by5 component
    
    * Add triple wide photo component
    
    * Add Mikoda Morning Market post
    
    * Add image to RSS feed

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,12 +1,18 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
+  const posts = await pagesGlobToRssItems(import.meta.glob('./posts/*.mdx'));
   return rss({
     title: 'Notes from Curtis...',
-    description: 'A collection of thoughts, memoirs, guides and photos from my life.',
+    description: 'A collection of thoughts, memoirs, guides and photos from my life ',
     site: context.site,
-    items: await pagesGlobToRssItems(
-      import.meta.glob('./posts/*.mdx')),
+    items: posts.map((post) => ({
+      title: post.title,
+      link: post.link,
+      pubDate: post.pubDate,
+      description: post.description,
+      enclosure: post.enclosure,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
